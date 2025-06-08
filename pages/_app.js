3d471950f3c7bccacc46b6ab68422580bb2971e1
Luiz Jelonschek <120672444+SonyBlackk@users.noreply.github.com>
2025-06-08 19:10:41 -0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
-import { inject } from '@vercel/speed-insights';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'],
+    subsets: ['latin'],
+    weight: ['400', '600', '700'],
 });
 
-inject();
-
 export default function App({ Component, pageProps }) {
-  return (
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-  );
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+            <SpeedInsights />
+        </main>
+    );
 }
