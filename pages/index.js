8f17c0e61a053e66c6337b0cc1f84962475baa38
Luiz Jelonschek <120672444+SonyBlackk@users.noreply.github.com>
2025-06-08 22:05:00 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -83,50 +83,12 @@ export default function Home() {
             <h2>Contato</h2>
             <div className="botoes-contato">
               <a
-                  href="https://github.com/SonyBlackk"
+                  href="contato.js"
                   target="_blank"
                   className="btn-contato github"
                   aria-label="GitHub"
                   rel="noreferrer">
                 <Image src="/github.svg" alt="GitHub" width={32} height={32} /> GitHub
-              </a>
-              <a
-                  href="https://www.linkedin.com/in/luiz-eduardo-jelonschek-919343265/"
-                  target="_blank"
-                  className="btn-contato linkedin"
-                  aria-label="LinkedIn"
-                  rel="noreferrer">
-                <Image src="/linkedin.svg" alt="LinkedIn" width={32} height={32} /> LinkedIn
-              </a>
-              <a
-                  href="https://www.instagram.com/eduardo_jelonschek/"
-                  target="_blank"
-                  className="btn-contato instagram"
-                  aria-label="Instagram"
-                  rel="noreferrer">
-                <Image src="/instagram.svg" alt="Instagram" width={32} height={32} /> Instagram
-              </a>
-              <a
-                  href="mailto:luizjelonschek07@gmail.com"
-                  className="btn-contato email"
-                  aria-label="Email">
-                <Image src="/email.svg" alt="Email" width={32} height={32} /> Email
-              </a>
-              <a
-                  href="https://wa.me/5546999174718"
-                  target="_blank"
-                  className="btn-contato whatsapp"
-                  aria-label="WhatsApp"
-                  rel="noreferrer">
-                <Image src="/whatsapp.svg" alt="WhatsApp" width={32} height={32} /> WhatsApp
-              </a>
-              <a
-                  href="https://open.spotify.com/user/31mnlmrgqqsgvdwwnj7ofmxv4tpy"
-                  target="_blank"
-                  className="btn-contato spotify"
-                  aria-label="Spotify"
-                  rel="noreferrer">
-                <Image src="/spotify.svg" alt="Spotify" width={32} height={32} /> Spotify
               </a>
             </div>
           </div>
