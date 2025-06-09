d1f1aaf23ad7e7cd4e0cf3d2877cfe6b5607b647
Luiz Jelonschek <120672444+SonyBlackk@users.noreply.github.com>
2025-06-08 22:20:41 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
   useEffect(() => {
@@ -82,12 +81,54 @@ export default function Home() {
         <section id="contato" className="contato">
           <div className="container">
             <h2>Contato</h2>
-            <Link href="/contato">
-            <a className="btn-contato contatos" aria-label="GitHub">
-              <Image src="/contato.svg" alt="GitHub" width={32} height={32} />
-              Meus Contatos
-            </a>
-          </Link>
+            <div className="botoes-contato">
+              <a
+                  href="https://github.com/SonyBlackk"
+                  target="_blank"
+                  className="btn-contato github"
+                  aria-label="GitHub"
+                  rel="noreferrer">
+                <Image src="/github.svg" alt="GitHub" width={32} height={32} /> GitHub
+              </a>
+              <a
+                  href="https://www.linkedin.com/in/luiz-eduardo-jelonschek-919343265/"
+                  target="_blank"
+                  className="btn-contato linkedin"
+                  aria-label="LinkedIn"
+                  rel="noreferrer">
+                <Image src="/linkedin.svg" alt="LinkedIn" width={32} height={32} /> LinkedIn
+              </a>
+              <a
+                  href="https://www.instagram.com/eduardo_jelonschek/"
+                  target="_blank"
+                  className="btn-contato instagram"
+                  aria-label="Instagram"
+                  rel="noreferrer">
+                <Image src="/instagram.svg" alt="Instagram" width={32} height={32} /> Instagram
+              </a>
+              <a
+                  href="mailto:luizjelonschek07@gmail.com"
+                  className="btn-contato email"
+                  aria-label="Email">
+                <Image src="/email.svg" alt="Email" width={32} height={32} /> Email
+              </a>
+              <a
+                  href="https://wa.me/5546999174718"
+                  target="_blank"
+                  className="btn-contato whatsapp"
+                  aria-label="WhatsApp"
+                  rel="noreferrer">
+                <Image src="/whatsapp.svg" alt="WhatsApp" width={32} height={32} /> WhatsApp
+              </a>
+              <a
+                  href="https://open.spotify.com/user/31mnlmrgqqsgvdwwnj7ofmxv4tpy"
+                  target="_blank"
+                  className="btn-contato spotify"
+                  aria-label="Spotify"
+                  rel="noreferrer">
+                <Image src="/spotify.svg" alt="Spotify" width={32} height={32} /> Spotify
+              </a>
+            </div>
           </div>
         </section>
 
